289daa8b3be8c6f3f917486d8c6224140be0d726
Swagat-patil <swagat@acuverconsulting.com>
2026-02-11 15:20:24 +0530

    Revert "Add files via upload"
    This reverts commit 9791358753b16783814eead381cf901bb2c25d3e.

--- output.tf
+++ output.tf
@@ -14,11 +14,3 @@ output "opensearch_serverless_endpoint" {
 output "opensearch_serverless_dashboard_url" {
   value = aws_opensearchserverless_collection.this.dashboard_endpoint
 }
-
-output "aoss_readonly_role_arn" {
-  value = aws_iam_role.aoss_readonly.arn
-}
-
-output "aoss_readwrite_role_arn" {
-  value = aws_iam_role.aoss_readwrite.arn
-}
